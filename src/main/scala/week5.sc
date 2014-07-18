8698f99b4962c31aeb3a1900f8bbbb640494a583
bamboo <avikbascra@gmail.com>
2014-07-18 13:00:26 +0300

    fuck , hate scala

---
+++ src/main/scala/week5.sc
@@ -0,0 +1,10 @@
+import week5.Week5
+
+
+Week5.removeAt(1, "123456".toList)
+Week5.removeAtNew(1, "123456".toList)
+
+
+
+Week5.flatten(List(List(1, 1), 2, List(3, List(5, 8))))
+
---
+++ src/main/scala/week5/Week5.scala
@@ -0,0 +1,26 @@
+package week5
+
+/**
+ * Created by bamboo on 18.07.14.
+ */
+object Main {
+
+}
+
+object Week5 {
+  def removeAt[T](n: Int, list: List[T]): List[T] = list match {
+    case Nil => Nil
+    case x :: xs =>
+      if (n == 0) xs
+      else x :: removeAt(n - 1, xs)
+  }
+
+  def removeAtNew[T](n: Int, list: List[T]): List[T] = (list take n) ::: (list drop n + 1)
+
+  def flatten(xs: List[Any]): List[Any] = xs match {
+    case Nil => Nil
+    case y :: ys =>
+      if (y.isInstanceOf[List[Any]]) flatten(y.asInstanceOf[List[Any]]) ::: flatten(ys)
+      else y :: flatten(ys)
+  }
+}
